c268d110cfa92ad733a0fdbe7d87dd01d09c5e91
Joschi96 <117238956+Joschi96@users.noreply.github.com>
2025-04-01 16:40:41 +0200

    first commit, project setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("Hello, Webpack!");
+import {  } from './modules/getData.js';
+import {  } from './modules/handleData.js';
+import {  } from './modules/renderData.js';
+import {  } from './modules/handleForm.js';
+
